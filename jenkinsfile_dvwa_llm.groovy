47405ae151e0a13dd254a20ae3b4cbb1098fe0df
kingofpings <khadke_aakash@yahoo.co.in>
2025-04-23 00:30:57 +0800

    sssssss

--- jenkinsfile_dvwa_llm.groovy
+++ jenkinsfile_dvwa_llm.groovy
@@ -18,7 +18,8 @@ pipeline {
                     pwd
                     whoami
                     ls -alh
-                    apt install python3
+                    sudo apt-get update
+                    sudo apt install python3
                     python3 --version
                     python3 -m venv env
                     source env/bin/activate
