c9bfed4e8b5c5dfa7cf991617407a6fc2b22b055
kingofpings <khadke_aakash@yahoo.co.in>
2025-04-23 00:04:34 +0800

    yo

---
+++ jenkinsfile_dvwa_llm.groovy
@@ -0,0 +1,37 @@
+
+pipeline {
+    agent {label "self"
+          }//end of agent
+
+    stages {
+        stage('Deploy_dvwa-llm') {
+            steps {
+                echo 'Deploying dvwa LLM'
+                sh '''
+                    cd dvwa-llm
+                    python3 -m venv env
+                    source env/bin/activate
+                    pip install -r requirements.txt
+                    pipenv install python-dotenv
+                
+                '''
+                echo 'Starting dvwa-llm'
+                sh '''
+                    cd dvwa-llm
+                    streamlit run main.py
+                
+                '''
+
+                echo 'Buidling dvwa-llm docker image'
+                sh '''
+                    cd dvwa-llm
+                    docker build -t dvla .
+                    docker run --env-file env.list -p 8501:8501 dvla
+                
+                '''
+
+            } //end of steps
+        } //end of stage build
+
+    } //end of stages
+} //end of pipeline
