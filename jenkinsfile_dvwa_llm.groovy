aafe7dfa886766877e03a7260ea383c00a0bad97
kingofpings <khadke_aakash@yahoo.co.in>
2025-04-23 00:29:06 +0800

    ddddd

--- jenkinsfile_dvwa_llm.groovy
+++ jenkinsfile_dvwa_llm.groovy
@@ -15,11 +15,11 @@ pipeline {
                 echo 'Deploying dvwa LLM'
                 sh '''
                     cd dvwa-llm
-                    python3 --version
                     pwd
                     whoami
                     ls -alh
                     apt install python3
+                    python3 --version
                     python3 -m venv env
                     source env/bin/activate
                     pip install -r requirements.txt
